db022510d1efdde8d2785a9881101d9d4de89a07
Victoria Kastanenka <vkastanenka@gmail.com>
2024-09-15 20:06:18 -0400

    setup in progress

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-primary':
+          'linear-gradient(225deg, #2A2A2E 11%, #2B125A 47%, #000000 85%);',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        purple: {
+          DEFAULT: '#3C1F7B',
+          dark: '#272430',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
